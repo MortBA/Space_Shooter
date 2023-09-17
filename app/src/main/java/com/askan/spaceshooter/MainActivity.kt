21d438bb421036256d9c51309c952f7c9f56c30b
BurakAskan <gusaskbu@student.gu.se>
2023-09-17 22:34:50 +0200

    Screen View of Scores

--- app/src/main/java/com/askan/spaceshooter/MainActivity.kt
+++ app/src/main/java/com/askan/spaceshooter/MainActivity.kt
@@ -10,14 +10,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.startGameButton)?.setOnClickListener {
-            val intent = Intent(this, GameActivity::class.java)
-            startActivity(intent)
-        }
+        showHighscore()
+
+        startGame()
+
+        scoresList()
+    }
+
+    fun showHighscore() {
         val prefs = getSharedPreferences(PREFS, MODE_PRIVATE)
         val maxDistanceTraveled = prefs.getInt(LONGEST_DIST, 0)
         val highscore = findViewById<TextView>(R.id.highscore)
         val tempHighScore = resources.getString(R.string.longest_traveled) + maxDistanceTraveled.toString()
         highscore.text = tempHighScore
+    }
+
+    fun scoresList() {
+        findViewById<Button>(R.id.scoresButton)?.setOnClickListener {
+            val intent = Intent(this, ScoresActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    fun startGame() {
+        findViewById<Button>(R.id.startGameButton)?.setOnClickListener {
+            val intent = Intent(this, GameActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
